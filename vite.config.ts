5b925b5f4052436537ffe24d7805d32a8cc775e0
RS4RT <contact.rs4rt@gmail.com>
2026-05-12 16:07:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/rs4rt/",
   server: {
     host: "::",
     port: 8080,
